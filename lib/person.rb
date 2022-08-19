77f83535860bdc70aa97ae5af358c57aba4a466e
Kelvin <vokemalosh32@gmail.com>
2022-08-19 15:28:14 +0300

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+    
+    class Person
+        #Class body
+    
+        #Instance method defination
+        def talk
+            puts "Hello World!"
+        end
+        def walk
+            puts "The Person is walking"    
+        end
+    
+    end
+    
